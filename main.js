d65d25d370630078ae939aa5bf2a878852df7389
Amohamed24 <mmohameda97@gmail.com>
2024-10-07 16:01:02 -0500

    Practiced a few things

---
+++ main.js
@@ -0,0 +1,37 @@
+// create a webpage with a 16x16 grid of square divs
+
+
+
+// const div = document.createElement("div");
+// div.setAttribute("flex", )
+
+// document.querySelector("container")
+
+
+// When the user inputs a number
+// Initialize a counter variable and set its value to zero
+// While counter is smaller than user inputted number increment the counter by one
+// Print the value of the counter variable
+
+// let answer = parseInt(prompt("Please enter a number "))
+
+
+// i want user to select a number
+// let num = parseInt(prompt("Please enter a number: "))
+
+
+// let equ = []
+
+// function add7() {
+//     const equ = num + 7
+//     console.log(`Your number is: ${num}`)
+//     console.log(`Your number plus 7: ${equ}`)
+
+// }
+
+// add7()
+
+
+// function named add7
+// take 1 num 
+// return that num + 7
