a5eb87bcbcbc5ffb3be337ba6f726c5cf0c6022e
Amohamed24 <mmohameda97@gmail.com>
2024-10-09 14:24:29 -0500

    added hover effects

--- main.js
+++ main.js
@@ -4,13 +4,33 @@
 
 
 
+
 const container = document.querySelector("#container"); 
 
 
 for (let i = 0; i<16; i++) {
-let gridsquares = document.createElement("div");
+const gridsquare = document.createElement("div");
+gridsquare.setAttribute("class", "squ") // create class to later call
 
-gridsquares.setAttribute("style", "width: 125px; height: 125px; border: 4px solid darkslategray; background: skyblue;");
-container.appendChild(gridsquares);
+
+gridsquare.setAttribute("style", "width: 125px; height: 125px; border: 4px solid darkslategray; background: skyblue;");
+container.appendChild(gridsquare);
 }
 
+// get all the square elements
+const squElements = document.querySelectorAll(".squ"); // select by class
+
+// add mouseover event listener to each square
+squElements.forEach((squ) => {
+    squ.addEventListener('mouseover', () => {
+        // change the background color
+        squ.style.backgroundColor = "yellow";
+    });
+});
+
+squElements.forEach((squ) => {
+    squ.addEventListener('mouseout', () => {
+        // change the background color
+        squ.style.backgroundColor = "skyblue";
+    });
+});
